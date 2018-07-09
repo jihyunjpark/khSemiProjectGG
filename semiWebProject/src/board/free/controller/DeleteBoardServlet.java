15f05f737ea408c9d6f6cf55ca0601d61a125a15
choiwon <choiwon@LAPTOP-QAQ7GR1E>
2018-07-10 01:37:59 +0900

    ㅎ

--- semiWebProject/src/board/free/controller/DeleteBoardServlet.java
+++ semiWebProject/src/board/free/controller/DeleteBoardServlet.java
@@ -14,28 +14,17 @@ import com.google.gson.Gson;
 import board.free.model.service.FreeService;
 import board.free.model.vo.FreeReplyVo;
 
-/**
- * Servlet implementation class DeleteReplyServlet
- */
 @WebServlet("/deleteBoardFree.do")
 public class DeleteBoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public DeleteBoardServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int boardNo = Integer.parseInt(request.getParameter("bno"));
 		
-		//비지니스 로직 호출
 		int result  = new FreeService().deleteBoard(boardNo);
 		
 		response.setContentType("application/json; charset=UTF-8"); 
--- semiWebProject/src/board/free/controller/DeleteReplyServlet.java
+++ semiWebProject/src/board/free/controller/DeleteReplyServlet.java
@@ -14,24 +14,14 @@ import com.google.gson.Gson;
 import board.free.model.service.FreeService;
 import board.free.model.vo.FreeReplyVo;
 
-/**
- * Servlet implementation class DeleteReplyServlet
- */
 @WebServlet("/deleteReply.do")
 public class DeleteReplyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public DeleteReplyServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int replyNo = Integer.parseInt(request.getParameter("replyNo"));
 		int boardNo = Integer.parseInt(request.getParameter("bNo"));
